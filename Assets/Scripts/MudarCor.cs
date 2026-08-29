cc45e29d1ee5135dce5e57cbbcbeb7c0843d34e6
Marcelojr122 <121398442+Marcelojr122@users.noreply.github.com>
2026-08-29 14:20:51 -0300

    Mudança de cor
    Terminei o código para mudar a cor do personagem

--- Assets/Scripts/MudarCor.cs
+++ Assets/Scripts/MudarCor.cs
@@ -3,42 +3,40 @@ using UnityEngine;
 
 public class MudarCor : MonoBehaviour
 {
-    private SpriteRenderer meuRender;
-    private float red;
-    private float blue;
-    private float green;
+    private Renderer meuRender;
+
+    [Range(0, 360)] private float hue;
 
     private float tempo = 0;
-    private bool estaDentro = false;
+    private bool estaFora = true;
 
     [Range(0, 1)] public float velocidadeMudaCor = 0.01f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        meuRender = GetComponent<SpriteRenderer>();
-        red = meuRender.color.r * 255;
-        green = meuRender.color.g * 255;
-        blue = meuRender.color.b * 255;
+        meuRender = GetComponent<Renderer>();
+        hue = meuRender.material.GetFloat("_Hue"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (estaDentro)
-        //{
-        //    MudandoCor(gameObject);
-        //}
+        if (estaFora && tempo >= velocidadeMudaCor)
+        {
+            MudandoCor(gameObject);
+            tempo = 0;
+        }
 
         tempo += Time.deltaTime;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        estaDentro = true;
-        if (tempo >= velocidadeMudaCor)
+        estaFora = false;
+        if (tempo >= velocidadeMudaCor && CompareTag("Tapete"))
         {
-            Debug.Log("Está entrando no circulo");
+            Debug.Log("Está no circulo");
 
             MudandoCor(collision.gameObject);
             
@@ -48,11 +46,41 @@ public class MudarCor : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        estaDentro = false;
+        estaFora = true;
     }
-
+    
     private void MudandoCor(GameObject objeto)
     {
+        var jHue = objeto.GetComponent<Renderer>().material.GetFloat("_Hue");
+        if (hue > jHue)
+        {
+            jHue += 1;
+        }
+        else if (hue < jHue)
+        {
+            jHue -= 1;
+        }
+
+        objeto.GetComponent<Renderer>().material.SetFloat("_Hue", jHue);
+    }
+}
+
+
+/*
+    (MudarCor)
+    private SpriteRenderer meuRender;
+    private float red;
+    private float blue;
+    private float green;  
+
+        (Start)
+        meuRender = GetComponent<SpriteRenderer>();
+        red = meuRender.color.r * 255;
+        green = meuRender.color.g * 255;
+        blue = meuRender.color.b * 255;
+
+        
+        (MudandoCor)
         float jR = objeto.GetComponent<SpriteRenderer>().color.r * 255;
         float jG = objeto.GetComponent<SpriteRenderer>().color.g * 255;
         float jB = objeto.GetComponent<SpriteRenderer>().color.b * 255;
@@ -85,5 +113,4 @@ public class MudarCor : MonoBehaviour
         }
 
         objeto.GetComponent<SpriteRenderer>().color = new Color(jR / 255f, jG / 255f, jB / 255f);
-    }
-}
+ */
